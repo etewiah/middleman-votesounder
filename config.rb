c0dfe4814b0a39636ccc1f04f2e425f63fd7d9b1
Ed Tewiah <etewiah@hotmail.cim>
2016-11-14 14:03:59 +0100

    About specific layout and css

--- config.rb
+++ config.rb
@@ -11,7 +11,7 @@ page '/*.txt', layout: false
 
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
-page "about.html", layout: :alpha_layout
+page "about.html", layout: :about_layout
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
